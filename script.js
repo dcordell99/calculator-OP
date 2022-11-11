5dfc59926a526d0e5daf08b45b98203b3c83addb
dcordell99 <davidcordell@hotmail.com>
2022-11-11 16:28:57 -0500

    Update index.html and style.css calculator design. Update script.js for basic functionality.

--- script.js
+++ script.js
@@ -1,8 +1,98 @@
-const add = (a, b) => a + b;
+let operate = {
+    "add": function(a, b) { return a + b; },
+    "subtract": function(a, b) { return a - b },
+    "multiply": function(a, b) { return a * b },
+    "divide": function(a, b) { return a / b }
+}
+
+
+
+// const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-const operate = (operator, a, b) => operator(a, b);
+// const operate = (operator, a, b) => operator(a, b);
 
-console.log(operate(add, 1, 3), operate(divide, 10, 2))
+let display = document.querySelector('.display');
+let numberBtns = document.querySelectorAll('.button-number');
+let operatorBtns = document.querySelectorAll('.buttons-operators');
+let equalsBtn = document.querySelector('.button-equals');
+let power = document.querySelector('.button-power');
+
+let secondNumber = false;
+
+let a;
+let b;
+let theOperator; 
+
+power.addEventListener('click', function() {
+    display.textContent = '';
+});
+
+numberBtns.forEach(number => {
+    number.addEventListener('click', (e) => {
+        if(display.textContent.length > 9) return;
+        if(secondNumber) display.textContent = '';
+        switch (e.target.getAttribute('id')) {
+            case 'zero': display.textContent = display.textContent += '0'; secondNumber = false;
+            break;
+            case 'one': display.textContent = display.textContent += '1';
+            secondNumber = false;
+            break;
+            case 'two': display.textContent = display.textContent += '2';
+            secondNumber = false;
+            break;
+            case 'three': display.textContent = display.textContent += '3';
+            secondNumber = false;
+            break;
+            case 'four': display.textContent = display.textContent += '4';
+            secondNumber = false;
+            break;
+            case 'five': display.textContent = display.textContent += '5';
+            secondNumber = false;
+            break;
+            case 'six': display.textContent = display.textContent += '6';
+            secondNumber = false;
+            break;
+            case 'seven': display.textContent = display.textContent += '7';
+            secondNumber = false;
+            break;
+            case 'eight': display.textContent = display.textContent += '8';
+            secondNumber = false;
+            break;
+            case 'nine': display.textContent = display.textContent += '9';
+            secondNumber = false;
+            break;
+        }
+    });
+});
+
+operatorBtns.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        if(display.textContent.length > 0) {
+            a = +display.textContent;
+            switch (e.target.getAttribute('id')) {
+                case 'add': theOperator = 'add';
+                break;
+                case 'subtract': theOperator = 'subtract';
+                break;
+                case 'multiply': theOperator = 'multiply';
+                break;
+                case 'divide': theOperator = 'divide';
+                break;
+            }
+            secondNumber = true;
+        } else {
+            return;
+        }
+        console.log(a, theOperator);
+    });
+});
+
+equalsBtn.addEventListener('click', () => {
+    if(display.textContent.length > 0) {
+        b = +display.textContent;
+        display.textContent = operate[theOperator](a,b);
+    }
+});
